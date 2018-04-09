42e1927c8e7b9e4be66f10ee155f4c493c509055
chenximing <243744327@qq.com>
2018-04-09 10:00:43 +0800

    修改

--- 数据结构与算法/code/16.cpp
+++ 数据结构与算法/code/16.cpp
@@ -1,23 +1,33 @@
+/*
+ * 1)循环n次，每次乘以x，时间复杂度：O(n)
+ * 2)使用一个变量m表示指数，如果m*2<n，则将结果平方，指数增长，时间复杂度：O(logn)
+ * 要考虑exponent=INT_MIN时，乘以-1会溢出，所以用long long保存指数
+ */
+
 class Solution {
 public:
     double Power(double base, int exponent) {
-        bool negativeExp = (exponent < 0);
+        bool b = exponent < 0;
         
-        if(base == 0)
-            return 0;
-        if(exponent == 0)
-            return 1;
+        if(base == 0)  return 0;
+        if(exponent == 0)  return 1;
         
-        if(exponent < 0)
-            exponent *= -1;
+        //需要使用一个long long类型的变量，因为当n为INT_MIN时，和-1乘会溢出
+        long long nl = exponent < 0 ? (long long)exponent * -1 : exponent;
+        double ret = 1.0;
         
-        double res = 1;
-        while(exponent--){
-            res *= base;
+        while(nl){
+            long long m = 1;//m必须也是long long,因为当m = INT_MAX/2+1时，计算m*2会溢出
+            double tp = base;
+            while(2 * m <= nl){
+                tp *= tp;
+                m *= 2;
+            }
+            nl = nl - m;
+            ret *= tp;
         }
         
-        if(negativeExp)
-            return 1/res;
-        return res;
+        ret = b ? 1 / ret : ret;
+        return ret;
     }
 };
--- 数据结构与算法/code/64.cpp
+++ 数据结构与算法/code/64.cpp
@@ -1,9 +1,13 @@
+/*
+ * 1)如下
+ * 2)构造函数
+ */
+
 class Solution {
 public:
     int Sum_Solution(int n) {
-        if(n <= 0)
-            return 0;
-        
-        return (1+n)*n>>1;
+        int ret = n;
+        ret && (ret = n + Sum_Solution(n - 1));
+        return ret;
     }
 };
